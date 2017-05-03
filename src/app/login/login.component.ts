29a9fa0693a05f7fcf13f878414f036b9bcc58f7
Bhavik Gosrani <Bhavik Gosrani>
2017-05-03 00:27:01 -0400

    app route

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+
+  const routes: Routes;
+
+  loginFunction() {
+    this.router.navigate(['/dashBoard']);
+  }
 
   ngOnInit() {
   }
